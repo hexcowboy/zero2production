ff9e22e8bea97067315e174f7354d05ef400192e
hexcowboy <accounts@cowboy.dev>
2022-06-12 15:40:13 -0700

    create subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct SubscribeFormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_form: web::Form<SubscribeFormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/healthcheck", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/healthcheck", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,17 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    // Create a new listener on port 0 which automatically chooses a random port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
+    // Retrieve the port from the listener
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to spawn the server.");
+    let _ = tokio::spawn(server);
+
+    format!("http:127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Start the application
@@ -19,14 +31,56 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    // Create a new listener on port 0 which automatically chooses a random port
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
-    // Retrieve the port from the listener
-    let port = listener.local_addr().unwrap().port();
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // Start the application
+    let address = spawn_app();
+    // Create a reqwest client
+    let client = reqwest::Client::new();
 
-    let server = zero2prod::run(listener).expect("Failed to spawn the server.");
-    let _ = tokio::spawn(server);
+    // Run the request
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscribe", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute the request.");
 
-    format!("http:127.0.0.1:{}", port)
+    // Make assertions
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_form_data() {
+    // Start the application
+    let address = spawn_app();
+    // Create a reqwest client
+    let client = reqwest::Client::new();
+    // Create a mapping of invalid data mapped to error message
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Run the request
+        let response = client
+            .post(&format!("{}/subscribe", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute the request.");
+
+        // Make assertions
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
